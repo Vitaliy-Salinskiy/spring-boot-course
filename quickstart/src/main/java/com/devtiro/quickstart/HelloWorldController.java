81fa7601095d10ea4a767095b85111cf30884bc5
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-05-05 04:17:04 +0200

    configurations

--- quickstart/src/main/java/com/devtiro/quickstart/HelloWorldController.java
+++ quickstart/src/main/java/com/devtiro/quickstart/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.devtiro.quickstart;
 
-import org.springframework,web,bind.annotatoin.RestController
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+     return "Hello, Java!";
+    }
+
 }
